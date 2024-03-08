be80e6057604d1f03002f9f9c36cb8a8c4c8e439
Prince Patel <pp9882602@gmail.com>
2024-03-08 20:43:43 +0530

    api_get_put_patch_delete Done

--- master/urls.py
+++ master/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('',taskListAPI,name='taskListAPI'),
+    path('task/<int:task_id>',tastDetailAPI,name='tastDetailAPI'),
 ]
--- master/views.py
+++ master/views.py
@@ -21,5 +21,26 @@ def taskListAPI(request):
         else:
             return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
 @api_view(['GET','PUT','PATCH','DELETE'])
-def tastDetailAPI(request):
-    pass
+def tastDetailAPI(request,task_id):
+    queryset = TaskModel.objects.get(id=task_id)
+    if request.method=="GET":
+        serializer = TastModelSerializer(queryset)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+    if request.method=="PUT":
+        serializer = TastModelSerializer(queryset,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method=="PATCH":
+        serializer = TastModelSerializer(queryset,request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    if request.method=="DELETE":
+        queryset.delete()
+        return Response({'message':"Data deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
